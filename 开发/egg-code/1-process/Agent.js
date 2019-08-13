6294c5b214caee66349d298e47deba11f3153863
Zardddddd60 <ldd_60@163.com>
2019-08-13 19:43:38 +0800

    feat: 进程守护

--- 开发/egg-code/1-process/Agent.js
+++ 开发/egg-code/1-process/Agent.js
@@ -3,6 +3,7 @@ module.exports = class Agent {
     constructor() {
         require('http').createServer((req, res) => {
             res.end('from agent!');
+            throw new Error('force quit.');
         }).listen(6060, () => {
             console.log('agent listening....');
         });
--- 开发/egg-code/1-process/master.js
+++ 开发/egg-code/1-process/master.js
@@ -8,6 +8,11 @@ class Master {
 
     forkAgent() {
         this.agent = fork(path.join(__dirname, './agent-entry.js'));
+        // 添加对agent进程exit事件的监听
+        this.agent.once('exit', () => {
+            console.log('fork a new agent...');
+            this.forkAgent();
+        });
     }
 }
 
