aa092c3210a70c848f2bea07811293826203e42b
Zardddddd60 <ldd_60@163.com>
2019-08-13 16:50:10 +0800

    feat: 使用类组织master

--- 开发/egg-code/1-process/master.js
+++ 开发/egg-code/1-process/master.js
@@ -1,4 +1,14 @@
 const { fork } = require('child_process');
 const path = require('path');
 
-fork(path.join(__dirname, './agent-entry.js'));
+class Master {
+    constructor() {
+        this.forkAgent();
+    }
+
+    forkAgent() {
+        this.agent = fork(path.join(__dirname, './agent-entry.js'));
+    }
+}
+
+new Master();
