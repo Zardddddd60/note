11526d5f2e4e7daebe0fd2ed7188739a309ca0fc
Zardddddd60 <ldd_60@163.com>
2019-08-14 10:16:01 +0800

    feat: 修改

--- 开发/egg-code/1-process/App.js
+++ 开发/egg-code/1-process/App.js
@@ -2,7 +2,8 @@ module.exports = class Agent {
     constructor(id) {
         require('http').createServer((req, res) => {
             res.end('from app!');
-            if (res.path.indexOf('favicon.icon') !== -1) {
+            console.log(req.url);
+            if (req.url.indexOf('favicon.ico') !== -1) {
                 throw new Error('app error');
             }
         }).listen(7188, () => {
